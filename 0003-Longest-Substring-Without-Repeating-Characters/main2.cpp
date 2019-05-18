3158ddda80bf696290c6d86c3eb3493ad2b6c43f
yanming <xuym0309@gmail.com>
2019-05-18 15:38:17 +0800

    method 2 and 3

---
+++ 0003-Longest-Substring-Without-Repeating-Characters/main2.cpp
@@ -0,0 +1,68 @@
+//
+// Created by xuyanming on 2019-05-18.
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+//
+
+/**
+Given a string, find the length of the longest substring without repeating characters.
+
+Example 1:
+
+Input: "abcabcbb"
+Output: 3 
+Explanation: The answer is "abc", with the length of 3. 
+Example 2:
+
+Input: "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3. 
+             Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+*/
+
+#include <iostream>
+#include <vector>
+#include <cassert>
+
+// Sliding Window
+// Another Implementation
+// Time Complexity: O(len(s))
+// Space Complexity: O(len(charset))
+class Solution {
+public:
+    int lengthOfLongestSubstring(std::string s) {
+        int freq[256] = {0};
+        int l = 0, r = -1; // slide windows: s[l,r]
+        int res = 0;
+        
+        while(r + 1 < s.size()) {
+            while(r + 1 < s.size() && freq[s[r+1]] == 0)
+                freq[s[++r]]++;
+
+            res = std::max(res, r - l+ 1);
+
+            if (r + 1 < s.size()) {
+                freq[s[++r]]++;
+                assert(freq[s[r]] == 2);
+                while(1<=r && freq[s[r]] == 2)
+                    freq[s[l++]]--;
+            }
+        }
+        return res;
+    }
+};
+
+
+
+int main() {
+    std::cout << Solution().lengthOfLongestSubstring( "abcabcbb" )<<std::endl; //3
+    std::cout << Solution().lengthOfLongestSubstring( "bbbbb" )<<std::endl;    //1
+    std::cout << Solution().lengthOfLongestSubstring( "pwwkew" )<<std::endl;   //3
+    std::cout << Solution().lengthOfLongestSubstring( "c" )<<std::endl;        //1
+    std::cout << Solution().lengthOfLongestSubstring( "" )<<std::endl;         //0
+    return 0;
+}
